6e0c44b070a6dde5ee716f4451cbff48443b8eab
Yuri Tretyakov <ytretyakov18@gmail.com>
2026-05-02 15:14:39 +0300

    tabs.lua: improve key bindings

--- lua/ytret/tabs.lua
+++ lua/ytret/tabs.lua
@@ -1,19 +1,33 @@
 -- Open/close a tab
-vim.keymap.set("n", "<leader>tt", ":tabnew<cr>")
-vim.keymap.set("n", "<leader>tq", ":tabclose<cr>")
+vim.keymap.set("n", "<leader>tt", function()
+    local bufname = vim.api.nvim_buf_get_name(0)
+    local has_file = bufname ~= ""
+    if has_file then
+        vim.cmd.tabnew("%")
+    else
+        vim.cmd.tabnew()
+    end
+end)
+vim.keymap.set("n", "<leader>tq", vim.cmd.tabclose)
+vim.keymap.set("n", "<leader>tc", vim.cmd.tabclose)
 
 -- Go to the prev/next/last tab
-vim.keymap.set("n", "<leader>tp", ":tabprev<cr>")
-vim.keymap.set("n", "<leader>tn", ":tabnext<cr>")
+vim.keymap.set("n", "<leader>tp", vim.cmd.tabprev)
+vim.keymap.set("n", "<leader>tn", vim.cmd.tabnext)
+vim.keymap.set("n", "<M-[>", vim.cmd.tabprev)
+vim.keymap.set("n", "<M-]>", vim.cmd.tabnext)
 vim.keymap.set("n", "<leader>tl", "g<tab>")
 
 -- Go to tab N
-vim.keymap.set("n", "<M-1>", ":tabn 1<cr>")
-vim.keymap.set("n", "<M-2>", ":tabn 2<cr>")
-vim.keymap.set("n", "<M-3>", ":tabn 3<cr>")
-vim.keymap.set("n", "<M-4>", ":tabn 4<cr>")
-vim.keymap.set("n", "<M-5>", ":tabn 5<cr>")
-vim.keymap.set("n", "<M-6>", ":tabn 6<cr>")
-vim.keymap.set("n", "<M-7>", ":tabn 7<cr>")
-vim.keymap.set("n", "<M-8>", ":tabn 8<cr>")
-vim.keymap.set("n", "<M-9>", ":tabn 9<cr>")
+local function gen_tabn(num)
+    return function() vim.cmd.tabn(string.format("%d", num)) end
+end
+vim.keymap.set("n", "<M-1>", gen_tabn(1))
+vim.keymap.set("n", "<M-2>", gen_tabn(2))
+vim.keymap.set("n", "<M-3>", gen_tabn(3))
+vim.keymap.set("n", "<M-4>", gen_tabn(4))
+vim.keymap.set("n", "<M-5>", gen_tabn(5))
+vim.keymap.set("n", "<M-6>", gen_tabn(6))
+vim.keymap.set("n", "<M-7>", gen_tabn(7))
+vim.keymap.set("n", "<M-8>", gen_tabn(8))
+vim.keymap.set("n", "<M-9>", gen_tabn(9))
