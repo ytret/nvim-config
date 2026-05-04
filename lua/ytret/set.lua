45c5edf38496942ea2d85481c545f4fb0d1076b0
Yuri Tretyakov <y.tretyakov@omp.ru>
2026-05-04 16:05:15 +0300

    set.lua: make auto strip of whitespace optional

--- lua/ytret/set.lua
+++ lua/ytret/set.lua
@@ -35,7 +35,9 @@ vim.opt.updatetime = 50
 -- vim.opt.colorcolumn = "80"
 -- Handled by smartcolumn.nvim
 
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-    pattern = { "*" },
-    command = [[%s/\s\+$//e]],
-})
+if YTRET_AUTOSTRIP == nil or YTRET_AUTOSTRIP == true then
+    vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+        pattern = { "*" },
+        command = [[%s/\s\+$//e]],
+    })
+end
