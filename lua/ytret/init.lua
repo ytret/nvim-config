fd7a801e1e8bc6879431171d5a3064e2063b76d5
Yuri Tretyakov <y.tretyakov@omp.ru>
2026-05-04 16:04:58 +0300

    Add optional local-pre.lua and local-post.lua

--- lua/ytret/init.lua
+++ lua/ytret/init.lua
@@ -1,9 +1,11 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+pcall(require, "ytret.local-pre")
+
 require("ytret.remap")
 require("ytret.lazy")
 require("ytret.set")
 require("ytret.tabs")
 
-pcall(require, "ytret.local")
+pcall(require, "ytret.local-post")
