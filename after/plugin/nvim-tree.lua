f81a511ad387b9020af587ff846bfd829acfb5f1
Yuri Tretyakov <ytretyakov18@gmail.com>
2026-05-24 09:54:14 +0300

    nvim-tree.lua: bind CTRL-T to a tab picker

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -1,3 +1,39 @@
+local DirectoryNode = require("nvim-tree.node.directory")
+local tabprompt = require("ytret.tabprompt")
+
+local function open_in_picked_tab(node)
+    local dir = node:as(DirectoryNode)
+    if dir then
+        dir:expand_or_collapse()
+        return
+    end
+
+    local path = node.absolute_path
+    if not path then
+        return
+    end
+
+    local result = tabprompt.prompt_for_tab({
+        allow_new = true,
+        getchar_prompt = "Tab (1-%d, N): ",
+        input_prompt = "Tab number (or N for new): ",
+        on_new = function()
+            vim.cmd.tabnew()
+            vim.cmd.edit(vim.fn.fnameescape(path))
+        end,
+    })
+
+    if not result then
+        return
+    end
+    if result.new then
+        return
+    end
+
+    vim.cmd.tabnext(tostring(result.tabnr))
+    vim.cmd.edit(vim.fn.fnameescape(path))
+end
+
 local function calc_size_pos()
     local gwidth = vim.api.nvim_list_uis()[1].width
     local gheight = vim.api.nvim_list_uis()[1].height
@@ -40,6 +76,17 @@ require("nvim-tree").setup({
     filters = {
         dotfiles = false,
     },
+    on_attach = function(bufnr)
+        local api = require("nvim-tree.api")
+        api.config.mappings.default_on_attach(bufnr)
+
+        vim.keymap.set("n", "<C-t>", function()
+            local node = api.tree.get_node_under_cursor()
+            if node then
+                open_in_picked_tab(node)
+            end
+        end, { buffer = bufnr, desc = "Open file in picked tab" })
+    end,
 })
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeFindFileToggle)
