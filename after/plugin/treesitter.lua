22c8b9bd556d8efa42ffb5e956faead3925e346b
Yuri Tretyakov <ytretyakov18@gmail.com>
2026-05-24 10:07:22 +0300

    treesitter.lua: use treesitter folding for FileTypes c and lua

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -17,3 +17,16 @@ require("nvim-treesitter").setup({
 vim.api.nvim_create_autocmd("FileType", {
     callback = function(args) pcall(vim.treesitter.start, args.buf) end,
 })
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = {
+        "c",
+        "lua",
+    },
+    callback = function(_)
+        -- vim.wo[0][0] ???
+        vim.opt.foldmethod = "expr"
+        vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+        vim.opt.foldlevel = 99
+        vim.opt.foldlevelstart = 99
+    end,
+})
