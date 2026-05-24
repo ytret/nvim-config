b3fd59ba3dab02f732b12e36c98bfd24c9e183d5
Yuri Tretyakov <ytretyakov18@gmail.com>
2026-05-24 16:00:02 +0300

    treesitter.lua: use folding for cpp

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -20,6 +20,7 @@ vim.api.nvim_create_autocmd("FileType", {
 vim.api.nvim_create_autocmd("FileType", {
     pattern = {
         "c",
+        "cpp",
         "lua",
     },
     callback = function(_)
