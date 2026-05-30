0b4a9ce3028ee8929e8fac45c2b55215b81bb4b2
Yuri Tretyakov <ytretyakov18@gmail.com>
2026-05-30 21:36:58 +0300

    lazy.lua: flash.nvim: remap jump to <c-s>

--- lua/ytret/lazy.lua
+++ lua/ytret/lazy.lua
@@ -148,7 +148,7 @@ require("lazy").setup({
             },
         },
         keys = {
-            { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end },
+            { "<c-s>", mode = { "n", "x", "o" }, function() require("flash").jump() end },
             { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end },
             { "r", mode = "o", function() require("flash").remote() end },
             { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end },
